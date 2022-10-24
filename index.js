e9869a687e86c9042366ba79540324b0f69f5a2b
CoSlava <vecojocari@gmail.com>
2022-10-24 16:02:21 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+let input = document.getElementById("input")
+input.value = 0
+
+let btn = document.getElementById("btn")
+
+let length = document.getElementById("length")
+let volume = document.getElementById("volume")
+let mass = document.getElementById("mass")
+
+btn.addEventListener("click", function() {
+    feet = input.value * 3.281
+    meters = input.value * 0.305
+    length.textContent = `${input.value} meters = ${feet.toFixed(2)} feet | ${input.value} feet = ${meters.toFixed(2)} meters`
+})
+
+btn.addEventListener("click", function() {
+    gallons = input.value * 0.264
+    liters = input.value * 3.785
+    volume.textContent = `${input.value} liters = ${gallons.toFixed(2)} gallons | ${input.value} gallons = ${liters.toFixed(2)} liters`
+})
+
+btn.addEventListener("click", function() {
+    pounds = input.value * 2.205
+    kilograms = input.value * 0.454
+    mass.textContent = `${input.value} kilograms = ${pounds.toFixed(2)} pounds | ${input.value} pounds = ${kilograms.toFixed(2)} kilograms`
+})
