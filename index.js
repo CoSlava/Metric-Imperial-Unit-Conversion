b97c38734e121b174726f86c98f2e9d4d216aa7e
CoSlava <vecojocari@gmail.com>
2022-10-24 16:08:40 +0300

    commit changes 1

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ let mass = document.getElementById("mass")
 btn.addEventListener("click", function() {
     feet = input.value * 3.281
     meters = input.value * 0.305
-    length.textContent = `${input.value} meters = ${feet.toFixed(2)} feet | ${input.value} feet = ${meters.toFixed(2)} meters`
+    length.textContent = `${input.value} meters ≈ ${feet.toFixed(2)} feet | ${input.value} feet ≈ ${meters.toFixed(2)} meters`
 })
 
 btn.addEventListener("click", function() {
     gallons = input.value * 0.264
     liters = input.value * 3.785
-    volume.textContent = `${input.value} liters = ${gallons.toFixed(2)} gallons | ${input.value} gallons = ${liters.toFixed(2)} liters`
+    volume.textContent = `${input.value} liters ≈ ${gallons.toFixed(2)} gallons | ${input.value} gallons ≈ ${liters.toFixed(2)} liters`
 })
 
 btn.addEventListener("click", function() {
     pounds = input.value * 2.205
     kilograms = input.value * 0.454
-    mass.textContent = `${input.value} kilograms = ${pounds.toFixed(2)} pounds | ${input.value} pounds = ${kilograms.toFixed(2)} kilograms`
+    mass.textContent = `${input.value} kilograms ≈ ${pounds.toFixed(2)} pounds | ${input.value} pounds ≈ ${kilograms.toFixed(2)} kilograms`
 })
